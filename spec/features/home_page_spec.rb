e53a0240cb812de7747ae90c192bfce011c37bf1
Alan Young <blankpages1@gmail.com>
2022-04-28 03:45:20 +0000

    tweaked tests:

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "HomePages", type: :feature do
-  scenario "The visitor should see projects" do
+  scenario "The visitor should see the portfolio home page" do
     visit root_path
-    expect(page).to have_text("Projects")
+    expect(page).to have_text("Alan's Portfolio")
   end
   
 end
